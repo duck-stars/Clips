396a96eb409ec96abd2810e5269a5b48dcc7a090
Seyed Yasin Mirhashemi <yasin.st2022@gmail.com>
2026-02-15 00:38:35 +0330

    Feat– make the 'Login' form functioanl

--- src/app/user/login/login.ts
+++ src/app/user/login/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { email, form, required, FormField } from '@angular/forms/signals';
+
+interface ILoginData {
+  email: string,
+  password: string
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  
+  loginModel = signal<ILoginData>({
+    email: '',
+    password: ''
+  })
 
+  loginForm = form(this.loginModel, (schemaPath) => {
+    // Apply validators on 'Email' field
+    required(schemaPath.email),
+    email(schemaPath.email),
+    // Apply validators on 'Password' field
+    required(schemaPath.password)
+  });
+
+  onSubmit(e: Event) {
+    console.log("Login opeartion completed!");
+    e.preventDefault();
+  }
 }
