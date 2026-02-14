24f18d3e289caddf90f1c387138a08a75bf90478
Seyed Yasin Mirhashemi <yasin.st2022@gmail.com>
2026-02-15 00:43:15 +0330

    Feat– created the 'Input' component for using as input fields in ReactiveForms

---
+++ src/app/shared/input/input.ts
@@ -0,0 +1,25 @@
+import { Component, input } from '@angular/core';
+import { ReactiveFormsModule, FormControl, AbstractControl } from '@angular/forms';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+
+@Component({
+  selector: 'app-input',
+  imports: [NgxMaskDirective, ReactiveFormsModule],
+  templateUrl: './input.html',
+  styleUrl: './input.css',
+  providers: [provideNgxMask()]
+})
+export class Input {
+  control = input.required<FormControl>();
+
+  name = input("Field");
+  type = input("text");
+  placeholder = input(`Enter ${this.name()}`);
+  fixedLength = input<number>(0);
+
+  patternError = input<string>(`${this.name()} is invalid.`);
+
+  mask = input<string>();
+
+  hideLabel = input(false);
+}
